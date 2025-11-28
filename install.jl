190f1d93cc6f3d557638d797d9adcbcee5012cb6
Jaewoo Joung <89329303+JaewooJoung@users.noreply.github.com>
2025-11-28 16:02:37 +0100

    Add installation script for Julia packages

---
+++ install.jl
@@ -0,0 +1,28 @@
+using Pkg
+
+# First, install all dependencies individually
+Pkg.add("HTTP")
+Pkg.add("JSON3") 
+Pkg.add("Gumbo")
+Pkg.add("Cascadia")
+Pkg.add("AbstractTrees")
+Pkg.add("WebDriver")
+Pkg.add("TOML")
+
+# Install Sendmail
+Pkg.add(url="https://github.com/JaewooJoung/Sendmail.jl")
+
+# Try to install Qprism in development mode
+Pkg.develop(url="https://github.com/JaewooJoung/Qprism.jl")
+
+# Force resolve dependencies
+Pkg.resolve()
+Pkg.instantiate()
+
+# Test if it works
+try
+    using Qprism
+    println("SUCCESS: Qprism is working!")
+catch e
+    println("ERROR: ", e)
+end
